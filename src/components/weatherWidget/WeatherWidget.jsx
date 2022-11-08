cf42161d0dde5ae6c3b7c1822c42de3bc3aab38a
jeromekithinji <kithinjijerome@gmail.com>
2022-11-08 22:10:47 +0000

    Weather widget done

--- src/components/weatherWidget/WeatherWidget.jsx
+++ src/components/weatherWidget/WeatherWidget.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './weatherWidget.scss'
+import { WiHumidity, WiDaySunny, WiThermometer } from "react-icons/wi";
+
 
 const WeatherWidget = () => {
     const [weatherStats, setWeatherStats] = useState(null); 
@@ -7,9 +9,6 @@ const WeatherWidget = () => {
     const [userLongitude, setUserLongitude] = useState(null);
 
     const successCallback = (position) => {
-        // console.log(position);
-        // console.log(position.coords.latitude);
-        // console.log(position.coords.longitude);
         setUserLongitude(position.coords.longitude);
         setUserLatitude(position.coords.latitude);
     };
@@ -27,7 +26,13 @@ const WeatherWidget = () => {
             .then((response) => response.json())
             .then((jsonResponse) => setWeatherStats(jsonResponse))
             .catch((err) => console.log(err));
-    };    
+    };
+
+    const date = new Date();
+
+    let minutes = date.getMinutes() < 11 ? "0" + date.getMinutes() : date.getMinutes();    
+
+    let time = date.getHours() + ":" + minutes;
 
     useEffect(() => {
         getWeatherStats();
@@ -35,11 +40,51 @@ const WeatherWidget = () => {
 
   return (
     <div className='widget'>
-        WeatherWidget
-        {weatherStats ? <h2 className="place">{weatherStats.location.name}, {weatherStats.location.country}</h2> : null}
-        {weatherStats ? <h2 className="weatherCondition">{weatherStats.current.condition.text}</h2> : null}  
+        <div className="widget-top">
+            {weatherStats ? <h2 className="widget-top__location">{weatherStats.location.name}, {weatherStats.location.country}</h2> : null}
+            {weatherStats ? <h2 className="widget-top__temp">{weatherStats.current.temp_c}&#176;C</h2> : null}
+            {weatherStats ? (
+            <img className="weatherIcon"
+                src={weatherStats.current.condition.icon}
+                alt={weatherStats.current.condition.text}
+            />
+        ) : null}
+        </div>
+        <div className="widget-bottom">
+            <div className="widget-bottom__property">
+                <div className="widget-bottom__property-header">
+                    <WiThermometer className="widget-bottom__property-header__icon"/> 
+                    <p className="widget-bottom__property-header__title">FEELS LIKE</p>
+                </div>
+                {weatherStats ? <p className="widget-bottom__property-stat">{weatherStats.current.feelslike_c}&#176;C</p> : null}
+            </div>
+            <div className="widget-bottom__property">
+                <div className="widget-bottom__property-header">
+                    <WiHumidity className="widget-bottom__property-header__icon"/> 
+                    <p className="widget-bottom__property-header__title">HUMIDITY</p>
+                </div>
+                {weatherStats ? <p className="widget-bottom__property-stat">{weatherStats.current.humidity}%</p> : null}
+            </div>
+            <div className="widget-bottom__property">
+                <div className="widget-bottom__property-header">
+                    <WiDaySunny className="widget-bottom__property-header__icon"/> 
+                    <p className="widget-bottom__property-header__title">UV INDEX</p>
+                </div>
+                {weatherStats ? <p className="widget-bottom__property-stat">{weatherStats.current.uv}</p> : null}
+            </div>
+        </div>
     </div>
   )
 }
 
 export default WeatherWidget
+
+// WeatherWidget
+// {weatherStats ? <h2 className="place">{weatherStats.location.name}, {weatherStats.location.country}</h2> : null}
+// {weatherStats ? <h2 className="weatherCondition">{weatherStats.current.condition.text}</h2> : null}  
+// {weatherStats ? (
+//             <img className="weatherIcon"
+//                 src={weatherStats.current.condition.icon}
+//                 alt={weatherStats.current.condition.text}
+//             />
+//         ) : null}
